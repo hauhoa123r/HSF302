626e0325b704593ea7ee409d673ca408033bd452
hauhoa123r <hoahvhe186810@fpt.edu.vn>
2025-05-26 20:51:07 +0700

    rename UserController

--- src/main/java/com/web/controller/UserController.java
+++ src/main/java/com/web/controller/UserController.java
@@ -4,9 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-
 @Controller
-public class HelloController {
+public class UserController {
 
     @GetMapping("")
     public String hello(Model model) {

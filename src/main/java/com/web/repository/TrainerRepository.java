35645a496dc46ceafd8d65de41f4cabf87604410
Bui Huy Hung <hunggk2004@gmail.com>
2025-05-26 21:23:35 +0700

    add foreign key

---
+++ src/main/java/com/web/repository/TrainerRepository.java
@@ -0,0 +1,8 @@
+package com.web.repository;
+
+import com.web.entity.TrainerEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TrainerRepository extends JpaRepository<TrainerEntity, Long> {
+
+}
---
+++ src/main/java/com/web/repository/custom/impl/ClassRepositoryImpl.java
@@ -0,0 +1,8 @@
+package com.web.repository.custom.impl;
+
+import com.web.entity.ClassEntity;
+import com.web.repository.ClassRepository;
+
+public class ClassRepositoryImpl  {
+
+}

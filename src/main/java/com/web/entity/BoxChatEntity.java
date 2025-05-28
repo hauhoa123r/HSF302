f1cda3dc7c56d0e8d0a93101abaef8d2356d10ff
Mr.NoBody <hieutvhe186966@fpt.edu.vn>
2025-05-28 16:25:40 +0700

    Create BoxChatEntity

---
+++ src/main/java/com/web/entity/BoxChatEntity.java
@@ -0,0 +1,25 @@
+package com.web.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "box_chats")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class BoxChatEntity {
+    @Column(name = "box_chat_id")
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "class_id", nullable = false)
+    private ClassEntity classEntity;
+}

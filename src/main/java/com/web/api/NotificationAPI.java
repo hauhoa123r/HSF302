9c76a592bdda9d4d3e65344427722f0357df8841
Mr.NoBody <hieutvhe186966@fpt.edu.vn>
2025-05-28 14:18:57 +0700

    Move all method from Controller to RestController

---
+++ src/main/java/com/web/api/NotificationAPI.java
@@ -0,0 +1,37 @@
+package com.web.api;
+
+import com.web.model.dto.NotificationDTO;
+import com.web.model.response.NotificationResponse;
+import com.web.service.NotificationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/notification")
+public class NotificationAPI {
+
+    private NotificationService notificationService;
+
+    @Autowired
+    public void setNotificationService(NotificationService notificationService) {
+        this.notificationService = notificationService;
+    }
+
+    @GetMapping("/getAll")
+    public List<NotificationResponse> getAllNotifications(@RequestBody NotificationDTO notificationDTO) {
+        return notificationService.getAllNotificationsByReceiverId(notificationDTO);
+    }
+
+    @GetMapping("/read")
+    public boolean markAsRead(@RequestBody NotificationDTO notificationDTO) {
+        notificationService.markAsRead(notificationDTO);
+        return true;
+    }
+
+    @PostMapping("/send")
+    public NotificationResponse sendNotification(@RequestBody NotificationDTO notificationDTO) {
+        return notificationService.sendNotification(notificationDTO);
+    }
+}

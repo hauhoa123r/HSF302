d5d90b8338b920bd4fcd92a9cab9948d91aa15ab
Mr.NoBody <hieutvhe186966@fpt.edu.vn>
2025-05-29 07:19:55 +0700

    Throw ErrorMappingException when object is null after converting

--- src/main/java/com/web/converter/MessageConverter.java
+++ src/main/java/com/web/converter/MessageConverter.java
@@ -2,10 +2,13 @@ package com.web.converter;
 
 import com.web.config.ModelMapperConfig;
 import com.web.entity.MessageEntity;
+import com.web.exception.mapping.ErrorMappingException;
 import com.web.model.dto.MessageDTO;
 import com.web.model.response.MessageResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.Optional;
 
 @Component
 public class MessageConverter {
@@ -18,10 +21,12 @@ public class MessageConverter {
     }
 
     public MessageEntity toEntity(MessageDTO messageDTO) {
-        return modelMapperConfig.modelMapper().map(messageDTO, MessageEntity.class);
+        Optional<MessageEntity> messageEntityOptional = Optional.ofNullable(modelMapperConfig.modelMapper().map(messageDTO, MessageEntity.class));
+        return messageEntityOptional.orElseThrow(() -> new ErrorMappingException(MessageDTO.class, MessageEntity.class));
     }
 
     public MessageResponse toResponse(MessageEntity messageEntity) {
-        return modelMapperConfig.modelMapper().map(messageEntity, MessageResponse.class);
+        Optional<MessageResponse> messageResponseOptional = Optional.ofNullable(modelMapperConfig.modelMapper().map(messageEntity, MessageResponse.class));
+        return messageResponseOptional.orElseThrow(() -> new ErrorMappingException(MessageEntity.class, MessageResponse.class));
     }
 }

f6009dbe91e867a3c9e75ba0fc943bbcddc183c4
Mr.NoBody <hieutvhe186966@fpt.edu.vn>
2025-05-31 15:44:28 +0700

    Apply database test

--- src/test/java/com/web/service/NotificationServiceIntegrationTest.java
+++ src/test/java/com/web/service/NotificationServiceIntegrationTest.java
@@ -7,6 +7,7 @@ import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.sql.Timestamp;
 import java.util.List;
@@ -14,6 +15,7 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
+@ActiveProfiles("test")
 @Transactional
 class NotificationServiceIntegrationTest {
     @Autowired

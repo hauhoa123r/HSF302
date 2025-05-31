3768fe8e3428d72593d5731bcdf0668ae9f63cac
Mr.NoBody <hieutvhe186966@fpt.edu.vn>
2025-05-31 16:14:35 +0700

    Add lombok

--- src/main/java/com/web/entity/AppliedPromotionEntity.java
+++ src/main/java/com/web/entity/AppliedPromotionEntity.java
@@ -1,11 +1,28 @@
 package com.web.entity;
 
-import jakarta.persistence.*;
-
 import java.sql.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "applied_promotions")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class AppliedPromotionEntity {
     @Column(name = "applied_id")
     @Id

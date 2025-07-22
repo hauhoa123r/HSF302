6a3730f1a2a8d0f013df462023cb85577de2be55
Mr.NoBody <hieutvhe186966@fpt.edu.vn>
2025-07-22 07:00:54 +0700

    enhance EntityNotFoundException with overloaded constructors for detailed error messages

--- src/main/java/com/web/exception/sql/EntityNotFoundException.java
+++ src/main/java/com/web/exception/sql/EntityNotFoundException.java
@@ -1,8 +1,12 @@
 package com.web.exception.sql;
 
 public class EntityNotFoundException extends RuntimeException {
-    public EntityNotFoundException(Class<?> clazz) {
-        super(String.format("Entity not found: %s", clazz.getSimpleName()));
+    public EntityNotFoundException(Class<?> entityClass) {
+        super(String.format("Entity of type %s not found", entityClass.getName()));
+    }
+
+    public EntityNotFoundException(Class<?> entityClass, Long id) {
+        super(String.format("Entity of type %s with ID %d not found", entityClass.getName(), id));
     }
 
     public EntityNotFoundException(String message) {

eb13f30583e08f408f9bd6dad719094776dc9261
Mr.NoBody <hieutvhe186966@fpt.edu.vn>
2025-07-23 08:12:36 +0700

    add AddUtils class for handling form submission and DTO preparation with toast notifications

---
+++ src/main/resources/templates/shared/assets/js/utils/add-utils.js
@@ -0,0 +1,47 @@
+import {FetchingUtils} from "/templates/shared/assets/js/utils/fetching-utils.js";
+import {FormDataUtils} from "/templates/shared/assets/js/utils/form-data.js";
+import toast from "/templates/shared/assets/js/utils/toast.js";
+
+export class AddUtils {
+    /**
+     * EditUtils constructor
+     * @param {string} formSelector
+     * @param dtoInstance
+     * @param {string} apiUrl
+     * @param {function} prepareDto
+     */
+    constructor({
+                    formSelector = "#form", dtoInstance, apiUrl, prepareDto
+                }) {
+        this.form = document.querySelector(formSelector);
+        this.dtoInstance = dtoInstance;
+        this.apiUrl = apiUrl;
+        this.prepareDto = prepareDto;
+    }
+
+    async setupFormEvents() {
+        if (!this.form) return;
+        this.form.addEventListener("submit", async (event) => {
+            event.preventDefault();
+            const formData = new FormData(this.form);
+            let dto = FormDataUtils.getObjectFromFormData(this.dtoInstance, formData);
+            if (this.prepareDto) {
+                dto = await this.prepareDto(dto);
+            }
+            const data = await FetchingUtils.fetch(`${this.apiUrl}`, {
+                method: "POST",
+                body: JSON.stringify(dto),
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            }, "text");
+            if (data !== null) {
+                toast.success("Thêm thành công", {
+                    progress: true,
+                    duration: 3000,
+                    icon: true
+                });
+            }
+        });
+    }
+}

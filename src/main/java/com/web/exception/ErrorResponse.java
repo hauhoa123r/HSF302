901f32219c4406039516bf0b2308caa9013ff06b
Mr.NoBody <hieutvhe186966@fpt.edu.vn>
2025-07-23 08:15:14 +0700

    add toastType and urlRedirect fields to ErrorResponse with new constructors for enhanced error handling

--- src/main/java/com/web/exception/ErrorResponse.java
+++ src/main/java/com/web/exception/ErrorResponse.java
@@ -9,6 +9,8 @@ import lombok.Setter;
 @NoArgsConstructor
 public class ErrorResponse extends RuntimeException {
     private int errorCode = 500; // Default error code
+    private String toastType = "error"; // Default toast type
+    private String urlRedirect; // Optional URL redirect, not used in this class
 
     public ErrorResponse(String message) {
         super(message);
@@ -17,5 +19,16 @@ public class ErrorResponse extends RuntimeException {
     public ErrorResponse(String message, int errorCode) {
         super(message);
         this.errorCode = errorCode;
+    }
+
+    public ErrorResponse(String toastType, String message) {
+        super(message);
+        this.toastType = toastType;
+    }
+
+    public ErrorResponse(String toastType, String message, String urlRedirect) {
+        super(message);
+        this.toastType = toastType;
+        this.urlRedirect = urlRedirect;
     }
 }
